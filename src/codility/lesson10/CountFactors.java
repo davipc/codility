46116aaa58ede342d97b94dacd445d03becf2ac9
rachelc <rachel.cavalcanti@gmail.com>
2016-03-06 19:36:13 -0300

    Added test to missing exercises. Also added some comments.

--- src/codility/lesson10/CountFactors.java
+++ src/codility/lesson10/CountFactors.java
@@ -3,6 +3,30 @@ package codility.lesson10;
 public class CountFactors {
 
 	/**
+	 * 
+A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
+
+For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
+
+Write a function:
+
+class Solution { public int solution(int N); }
+
+that, given a positive integer N, returns the number of its factors.
+
+For example, given N = 24, the function should return 8, because 24 has 8 factors, namely 1, 2, 3, 4, 6, 8, 12, 24. There are no other factors of 24.
+
+Assume that:
+
+N is an integer within the range [1..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(sqrt(N));
+expected worst-case space complexity is O(1).
+
+	 * 
+	 * 
+	 * 
 	 * For each divisor found there is also a symmetric divisor (for instance, N=6, divisor 2 has symmetric 3)
 	 * So, every time we find a divisor below sqrt(N), we also count the symmetric one. If symmetric is the same as 
 	 * the divisor itself, count only once (when the divisor is the same as sqrt(N))
