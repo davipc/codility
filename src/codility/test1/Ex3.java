7050f83b8310be725806af161c2bec91c20b49c0
rachelc <rachel.cavalcanti@gmail.com>
2016-03-14 11:20:39 -0300

    Added comments

--- src/codility/test1/Ex3.java
+++ src/codility/test1/Ex3.java
@@ -1,26 +1,29 @@
 package codility.test1;
 
+
 /**
  * 
- * 
+ * Problem: A knight is positioned at square (0,0) in an infinite sized chess board. 
+ * Calculate how many moves will be necessary to move the knight to position (X,Y).
  * 
  * @author Davi
  *
  */
 public class Ex3 {
 
-	    public int solution(int N) {
+	    public int solution(int X, int Y) {
 	        int result = -1;
 	    	
-	    	return result;
+	        
+	        System.out.println("(" + X + "," + Y + ") => " + result + " moves");
+
+	        return result;
 	    }
 
 	    public static void main(String[] args) {
 	    	
 	    	Ex3 ex1 = new Ex3();
-	    	
-	    	ex1.solution(328);
-	    	ex1.solution(1162);
+	    	ex1.solution(0, 0);
 		}
 
 }
